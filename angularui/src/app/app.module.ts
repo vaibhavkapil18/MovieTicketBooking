2ccfe59518e0c67c5222f5558edfef0ae451175a
vaibhavkapil18 <137314421+vaibhavkapil18@users.noreply.github.com>
2023-10-30 22:02:04 +0530

    Adding Image slider

--- angularui/src/app/app.module.ts
+++ angularui/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -10,10 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { LocationSelectorComponent } from './location-selector/location-selector.component';
-import { CinemaComponent } from './components/cinema/cinema.component';
 import { FormsModule } from '@angular/forms';
-
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,8 +18,6 @@ import { FormsModule } from '@angular/forms';
     DashboardComponent,
     NavigationComponent,
     LocationSelectorComponent,
-    CinemaComponent,
-    
   ],
   imports: [
     BrowserModule,
